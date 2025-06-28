938d2117921668085d49fa9351a6c8a629d134ea
tejasvee <tejasveegupta08@gmail.com>
2025-06-28 14:51:21 +0530

    initial commmit,created small project of cars,added model

---
+++ models/carData.js
@@ -0,0 +1,21 @@
+
+var cars = [
+    {
+        name: "Swift",
+        company: "maruti suzuki"
+    },
+    {
+        name: "Kia Carens",
+        company: "Kia"
+    },
+    {
+        name: "thar",
+        company: "mahindra"
+    },
+    {
+        name: "innova",
+        company: "Toyota"
+    }
+];
+
+module.exports = cars;
